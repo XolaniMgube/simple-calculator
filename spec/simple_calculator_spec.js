ae85f1c3d553129b7e248dc6000df62bc618b121
xolani <xolani.mgube@umuzi.org>
2020-01-07 16:11:29 +0200

    files restructuring

---
+++ spec/simple_calculator_spec.js
@@ -0,0 +1,24 @@
+let calculatorTest = require("../src/simple_calculator.js");
+
+let add = calculatorTest.add;
+let multiply = calculatorTest.multiply;
+
+describe("addition calculator", () => {
+    it("adds two numbers together", () => {
+        expect(add(1,2)).toBe(3);
+    }) 
+
+    it("add multiple numbers together", () =>{
+        expect(add(1,2,3,4)).toBe(10);
+    })
+});
+
+describe("multiplication calculator", () =>{
+    it("multiplies two numbers", () =>{
+        expect(multiply(2,2)).toBe(4);
+    })
+
+    it("multiplies multiple numbers", () =>{
+        expect(multiply(1,2,3,4)).toBe(24);
+    })
+});
